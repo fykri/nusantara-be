47f47f636f192d7f22f2bdef97339fb9c23eb4fb
fykri <dzulfikriyunus@gmail.com>
2024-06-21 23:13:32 +0800

    fix cors

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(
 );
 const port = process.env.PORT;
 app.use(cookie());
-app.use(cors({credentials: true, origin:'http://localhost:3000'}));
+app.use(cors({credentials: true, origin:'http://localhost:5173'}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
